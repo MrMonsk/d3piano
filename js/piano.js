06a748b572203b918d8cdb54dafaf9618ddbb73a
bcrowell <brettcrowell@gmail.com>
2016-03-12 17:40:06 -0500

    learning up

--- js/piano.js
+++ js/piano.js
@@ -1,43 +1,42 @@
 (function(){
 
-  var keyWidth = 25;
-  var keyHeight = 140;
+  var keyHeight = 200;
+  var keyWidth = keyHeight * .2;
   var blackKeyHeight = keyHeight * 0.65;
   var blackKeyWidth = keyWidth * 0.5;
-  var rounding = keyWidth * 0.2;
+  var rounding = keyWidth * 0.1;
 
   var sortedNoteData = notes.sort(function(a, b){
     return (b.isBlack) ? -1 : 1;
   });
 
   var svg = d3.select("svg").attr({
+
     width: keyWidth * sortedNoteData.filter(function(note){ return !note.isBlack; }).length,
     height: keyHeight
+
   });
 
+  var keys = svg.selectAll("rect").data(sortedNoteData, function(d) { return d.keyNumber; });
 
-
-  var circle = svg.selectAll("rect")
-    .data(sortedNoteData, function(d) { return d.keyNumber; });
-
-  circle.enter().append("rect")
+  keys.enter().append("rect")
     .attr({
       rx: rounding,
-      ry: rounding
-    })
-    .attr("y", 0)
-    .attr("x", function(d, i) { return d.keyPosition * keyWidth; })
-    .attr("fill", function(d){
-      return (d.isBlack) ? 'black' : 'white'
-    })
-    .attr("width", function(d,i){
-      return (d.isBlack) ? blackKeyWidth : keyWidth;
-    })
-    .attr("height", function(d,i){
-      return (d.isBlack) ? blackKeyHeight : keyHeight;
-    })
-    .attr("stroke", "#EEEEEE")
+      ry: rounding,
+      y: 0,
+      x: function(d, i) { return d.keyPosition * keyWidth; },
+      fill: function(d){
+        return (d.isBlack) ? 'black' : 'white'
+      },
+      width:  function(d,i){
+        return (d.isBlack) ? blackKeyWidth : keyWidth;
+      },
+      height: function(d,i){
+        return (d.isBlack) ? blackKeyHeight : keyHeight;
+      },
+      stroke: "#AAAAAA"
+    });
 
-  circle.exit().remove();
+  keys.exit().remove();
 
 })();
