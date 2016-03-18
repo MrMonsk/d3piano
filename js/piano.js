407026a86fae212aacbad7cc70ddcd72d3639904
bcrowell <brettcrowell@gmail.com>
2016-03-17 21:42:06 -0400

    cleaning up

--- js/piano.js
+++ js/piano.js
@@ -10,33 +10,63 @@
     return (b.isBlack) ? -1 : 1;
   });
 
-  var svg = d3.select("svg").attr({
+  function draw(){
 
-    width: keyWidth * sortedNoteData.filter(function(note){ return !note.isBlack; }).length,
-    height: keyHeight
+    var svg = d3.select("svg").attr({
 
-  });
+      width: keyWidth * sortedNoteData.filter(function(note){ return !note.isBlack; }).length,
+      height: keyHeight
 
-  var keys = svg.selectAll("rect").data(sortedNoteData, function(d) { return d.keyNumber; });
-
-  keys.enter().append("rect")
-    .attr({
-      rx: rounding,
-      ry: rounding,
-      y: 0,
-      x: function(d, i) { return d.keyPosition * keyWidth; },
-      fill: function(d){
-        return (d.isBlack) ? 'black' : 'white'
-      },
-      width:  function(d,i){
-        return (d.isBlack) ? blackKeyWidth : keyWidth;
-      },
-      height: function(d,i){
-        return (d.isBlack) ? blackKeyHeight : keyHeight;
-      },
-      stroke: "#AAAAAA"
     });
 
-  keys.exit().remove();
+    var keys = svg.selectAll("rect").data(sortedNoteData, function(d) { return d.keyNumber; });
+
+    keys
+      .enter()
+      .append("rect")
+      .attr({
+        rx: rounding,
+        ry: rounding,
+        y: 0,
+        x: function(d, i) {
+          return d.keyPosition * keyWidth;
+        },
+        width:  function(d,i){
+          return (d.isBlack) ? blackKeyWidth : keyWidth;
+        },
+        height: function(d,i){
+          return (d.isBlack) ? blackKeyHeight : keyHeight;
+        },
+        stroke: "#AAAAAA",
+
+      })
+      .on({
+        mouseenter: function(note){
+          note.hover = true;
+          draw();
+        },
+        mouseleave: function(note){
+          note.hover = false;
+          draw();
+        }
+      });
+
+    keys
+      .attr({
+        fill: function(d){
+          if(d.isBlack){
+            return (d.hover) ? '#444444' : '#000000';
+          } else {
+            return (d.hover) ? '#eeeeee' : '#ffffff';
+          }
+        }
+      });
+
+    keys.exit().remove();
+
+  }
+
+  draw();
 
 })();
+
