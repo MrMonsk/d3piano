6fc9ae74df8d79691867bf4b5ecc9c6409d31ade
bcrowell <brettcrowell@gmail.com>
2016-03-24 22:10:04 -0400

    adding basic vexflow example

--- js/piano.js
+++ js/piano.js
@@ -1,6 +1,48 @@
 (function(){
 
-  // MIDI.js
+  // VEXFLOW /////////////////////////////////////////////////////////////////
+
+  var canvas = document.getElementById("vexflow");
+  var renderer = new Vex.Flow.Renderer(canvas, Vex.Flow.Renderer.Backends.CANVAS);
+
+  var ctx = renderer.getContext();
+  var stave = new Vex.Flow.Stave(10, 0, 500);
+  stave.addClef("treble").setContext(ctx).draw();
+
+  // Create the notes
+  var vexNotes = [
+    // A quarter-note C.
+    new Vex.Flow.StaveNote({ keys: ["c/4"], duration: "q" }),
+
+    // A quarter-note D.
+    new Vex.Flow.StaveNote({ keys: ["d/4"], duration: "q" }),
+
+    // A quarter-note rest. Note that the key (b/4) specifies the vertical
+    // position of the rest.
+    new Vex.Flow.StaveNote({ keys: ["b/4"], duration: "qr" }),
+
+    // A C-Major chord.
+    new Vex.Flow.StaveNote({ keys: ["c/4", "e/4", "g/4"], duration: "q" })
+  ];
+
+  // Create a voice in 4/4
+  var voice = new Vex.Flow.Voice({
+    num_beats: 4,
+    beat_value: 4,
+    resolution: Vex.Flow.RESOLUTION
+  });
+
+  // Add notes to voice
+  voice.addTickables(vexNotes);
+
+  // Format and justify the notes to 500 pixels
+  var formatter = new Vex.Flow.Formatter().
+    joinVoices([voice]).format([voice], 500);
+
+  // Render voice
+  voice.draw(ctx, stave);
+
+  // MIDI.js /////////////////////////////////////////////////////////////////
 
   MIDI.loadPlugin({
     soundfontUrl: "./bower_components/midi-js-soundfonts/FluidR3_GM/",
@@ -13,7 +55,7 @@
     }
   });
 
-  // PIANO
+  // PIANO /////////////////////////////////////////////////////////////////
 
   var keyHeight = 200;
   var keyWidth = keyHeight * .2;
@@ -44,7 +86,7 @@
 
   function draw(){
 
-    var svg = d3.select("svg").attr({
+    var svg = d3.select("svg#piano").attr({
 
       width: keyWidth * sortedNoteData.filter(function(note){ return !note.isBlack; }).length,
       height: keyHeight
